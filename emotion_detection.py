15f5d5fe57f2b1e2d8f956bd96ff1e4535ec9dcd
jcgarridofps <jcgarrido.fps@outlook.com>
2025-02-09 08:01:29 -0500

    Task 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,6 +1,7 @@
 '''
 Emotion detector module
 '''
+import json
 import requests
 
 def emotion_detector(text_to_analyze):
@@ -8,11 +9,23 @@ def emotion_detector(text_to_analyze):
     Emotion detector function
     '''
 
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/\
-            watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/' + \
+            'watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     data = { "raw_document": { "text": text_to_analyze } }
 
     response = requests.post(url, json=data, headers=header, timeout = 10)
+    response_json = json.loads(response.text)
+    emotions = response_json["emotionPredictions"][0]["emotion"]
+    dominant_emotions = [k for k in emotions.keys() if emotions[k] == max(emotions.values())]
 
-    return response.text
+    ret = {
+        "anger": emotions["anger"],
+        "disgust": emotions["disgust"],
+        "fear": emotions["fear"],
+        "joy": emotions["joy"],
+        "sadness": emotions["sadness"],
+        "dominant_emotion": dominant_emotions[0],
+    }
+
+    return ret
